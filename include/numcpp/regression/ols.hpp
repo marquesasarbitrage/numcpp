d0ea9f7eb054fa5f0327633f9fd844b968239ffc
vannavolgaa <marquesasaribitrage@gmail.com>
2026-03-07 00:44:05 +0100

    ols dev + tests

--- include/numcpp/regression/ols.hpp
+++ include/numcpp/regression/ols.hpp
@@ -1,20 +1,34 @@
 #pragma once 
-#include <vector>
+#include "Eigen/Core"
 #include <numcpp/objects/base.hpp>
+#include <numcpp/errors.hpp>
+#include <numcpp/probability/tstudent.hpp>
 
-namespace numccp {
+namespace numcpp {
 
     namespace regression {
 
         namespace olstools {
 
+            inline void checkInput(const numcpp::objects::Vector& Y,const numcpp::objects::Matrix& X) {
+
+                if(Y.size() != X.rows()) errors::throwInvalidInputr("The size of the vector Y must be equal to the number of rows from matrix X in OLS");
+            }
+
             inline numcpp::objects::Matrix addIntercept(const numcpp::objects::Matrix& X, bool fitIntercept) {
 
                 if (!fitIntercept) return X;
                 Eigen::MatrixXd XX(X.rows(), X.cols() + 1);
-                XX.col(0) = Eigen::VectorXd::Ones(X.rows()); // first column = ones
+                XX.col(0) = Eigen::VectorXd::Ones(X.rows());
                 XX.block(0, 1, X.rows(), X.cols()) = X;
                 return XX;
+            }
+
+            inline numcpp::objects::Matrix inverseXtX(const numcpp::objects::Matrix& X) {
+
+                Eigen::MatrixXd XtX = X.transpose() * X;
+                Eigen::LDLT<Eigen::MatrixXd> ldlt(XtX);
+                return ldlt.solve(Eigen::MatrixXd::Identity(XtX.rows(), XtX.rows()));
             }
 
             inline numcpp::objects::Matrix projectionMatrix(const numcpp::objects::Matrix& X) {
@@ -24,17 +38,78 @@ namespace numccp {
                 return X * ldlt.solve(X.transpose());
             }
 
+
         }
 
-        struct OrdinaryLeastSquare {
+        struct OLS {
             bool useIntercept; 
-            std::vector<double> betas; 
+            numcpp::objects::Vector betas; 
             numcpp::objects::Vector residuals; 
-            double rSquared; 
-            std::vector<double> standardError; 
+            numcpp::objects::Matrix inverseXtX;
+            double sampleVariance;
+            double rSquared;
         
-            std::vector<double> confidenceInterval(bool upper);
+            inline int numberParameters() const {return betas.size();}
+
+            inline int numberObservations() const {return residuals.size();}
+
+            inline int degreesFreedom() const {return numberObservations()-numberParameters();}
+
+            inline Eigen::VectorXd standardErrors() const {return (sampleVariance * inverseXtX.diagonal()).array().sqrt();}
+
+            inline Eigen::VectorXd confidenceInterval(double quantile, bool upper) {
+
+                double tcrit = probability::invCdf(probability::Student{double(degreesFreedom())},0.5 + quantile / 2.0);
+                Eigen::VectorXd ci(numberParameters());
+                Eigen::VectorXd se = standardErrors();
+                for(int j = 0; j < numberParameters(); ++j) {
+                    ci(j) = betas(j) + (upper ? tcrit * se(j) : -tcrit * se(j));
+                }
+
+                return ci;
+            }
+
+            inline Eigen::VectorXd tstats() {
+
+                Eigen::VectorXd ts(numberParameters()); 
+                Eigen::VectorXd se = standardErrors();
+                for(int j = 0; j < numberParameters(); ++j) {
+                    ts(j) = betas(j)/se(j);
+                }
+                return ts;
+            }
+
+            inline double adjustedRSquared() const {return 1.0 - (1.0 - rSquared) * (numberObservations() - 1.0) / double(degreesFreedom());}
+
+            inline Eigen::VectorXd pValues() {
+
+                Eigen::VectorXd tstats_ = tstats();
+                Eigen::VectorXd p(tstats_.size());
+
+                for(int i = 0; i < tstats_.size(); ++i) {
+                    p(i) = 2.0 * (1.0 - cdf(probability::Student{double(degreesFreedom())}, std::abs(tstats_(i))));
+                }
+
+                return p;
+            }
         };
+
+
+        inline OLS ols(const numcpp::objects::Vector& Y,const numcpp::objects::Matrix& X, bool fitIntercept) {
+
+            numcpp::objects::Matrix X_ = olstools::addIntercept(X, fitIntercept); 
+            numcpp::objects::Matrix XtXinv = olstools::inverseXtX(X_);
+            numcpp::objects::Vector betas = XtXinv*X_.transpose()*Y;
+            numcpp::objects::Vector residuals = Y - X_*betas;
+            double rss = residuals.dot(residuals);
+            double sampleVariance = rss / double(Y.size() - betas.size());
+            double ymean = Y.mean();
+            double tss = 0.0;
+            for (size_t i = 0; i < Y.size(); ++i){double d = Y[i] - ymean;tss += d * d;}
+            double rsquared = 1.0 - rss / tss;
+            return {fitIntercept,betas,residuals,XtXinv,sampleVariance,rsquared};
+        }
+
 
         
 
--- tests/ols.cpp
+++ tests/ols.cpp
@@ -1,38 +1,53 @@
 #include <numcpp/regression/ols.hpp>
 #include <iostream>
+#include "numcpp/objects/base.hpp"
+#include "utils.hpp"
 
 
-numcpp::objects::Vector getY() {
+void testHeightsWeightsExampleWiki() {
 
     numcpp::objects::Vector y(15); 
     y << 52.21,53.12,54.48,55.84,57.20,58.57,59.93,61.29,63.11,64.47,66.28,68.10,69.92,72.19,74.46;
-    return y;
+
+    numcpp::objects::Matrix X(15,2);
+    double heights[] = {1.47,1.50,1.52,1.55,1.57,1.6,1.63,1.65,1.68,1.70,1.73,1.75,1.78,1.80,1.83};
+
+    for(int i=0;i<15;i++){
+        X(i,0) = heights[i];
+        X(i,1) = heights[i]*heights[i];
+    }
+
+    numcpp::regression::OLS ols = numcpp::regression::ols(y, X, true);
+
+    
+    assert(isClose(ols.betas(0), 128.8128, 1e-4));
+    assert(isClose(ols.betas(1), -143.1620, 1e-4));
+    assert(isClose(ols.betas(2), 61.9603, 1e-4));
+
+    numcpp::objects::Vector se = ols.standardErrors();
+    assert(isClose(se(0), 16.3083, 1e-4));
+    assert(isClose(se(1), 19.8332, 1e-4));
+    assert(isClose(se(2), 6.0084, 1e-4));
+
+    numcpp::objects::Vector ts = ols.tstats();
+    assert(isClose(ts(0), 7.8986, 1e-4));
+    assert(isClose(ts(1), -7.2183, 1e-4));
+    assert(isClose(ts(2), 10.3122, 1e-4));
+
+    numcpp::objects::Vector pv = ols.pValues();
+    assert(isClose(pv(0), 0.0, 1e-4));
+    assert(isClose(pv(1), 0.0, 1e-4));
+    assert(isClose(pv(2), 0.0, 1e-4));
+
+    assert(isClose(ols.rSquared,0.9989,1e-4));
+    assert(isClose(ols.adjustedRSquared(),0.9987,1e-4));
+
 }
 
-numcpp::objects::Matrix getX() {
-
-    numcpp::objects::Vector x(15,2); 
-    x << 1.47, 1.47*1.47, 
-    1.50, 1.50*1.50,
-    1.52, 1.52*1.52,
-    1.55, 1.55*1.55,
-    1.57, 1.57*1.57,
-    1.60, 1.60*1.60,
-    1.63, 1.63*1.63,
-    1.65, 1.65*1.65,
-    1.68, 1.68*1.68,
-    1.70, 1.70*1.70,
-    1.73, 1.73*1.73,
-    1.75, 1.75*1.75,
-    1.78, 1.78*1.78,
-    1.80, 1.80*1.80,
-    1.83, 1.83*1.83;
-
-    return x;
-}
 
 int main() {
 
-
-    std::cout << numcpp::regression::olstools::projectionMatrix(getX()) << std::endl;
+    testHeightsWeightsExampleWiki(); 
+    std::cout << "All tests for the OLS object have been passed successfully!" << std::endl;
+    return 0;
 }
